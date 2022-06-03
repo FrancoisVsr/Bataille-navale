be3e6d138bb5986f82215bd62a50bb55d1aaec77
AUFFRAY Jean <jean.auffray@altran.com>
2022-06-03 10:00:21 +0200

    add files case, game, joueur, update main

---
+++ src/game.h
@@ -0,0 +1,19 @@
+/**
+ * @author      Auffray
+ * @date        25/05/2022
+ * @version     v0.01
+ * @brief       Librairie pour les fonctions de jeux
+ */
+
+#ifndef GAME_H
+#define GAME_H
+
+// Fonctions qui gère le déroulement d'une partie
+
+void start();
+int menu();
+bool GameLoop_1_player();
+bool GameLoop_2_player();
+void clean_display();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include "game.h"
 
 int main() {
-
-    std::cout << "Hello World !" << std::endl;
+    char choice = 'n';
+    do {
+        start();
+        int choix_mode = menu();
+        if(choix_mode == 2) {
+            if(GameLoop_2_player()) {
+                std::cout << "Gagnant : Player1" << std::endl;
+            }
+            else{
+                std::cout << "Gagnant : Player2" << std::endl;
+            }
+        }
+        else {
+            if(GameLoop_1_player()) {
+                std::cout << "Gagnant : playeur" << std::endl;
+            }
+            else{
+                std::cout << "Gagant : IA" << std::endl;
+            }
+        }
+        do {
+            std::cout << "Do you want to replay ? [y/n] : ";
+            std::cin >> choice;
+        }while(choice != 'n' && choice != 'y');
+    }while(choice == 'y');    
     return 0;
 }
