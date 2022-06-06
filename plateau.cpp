8bd2ce942fdbe3319e85ad39c0237f812fed3125
Yslachy <corentin.gauthier@yahoo.com>
2022-06-06 11:39:22 +0200

    Gestion conflits placement bateau avec directions OK

--- plateau.cpp
+++ plateau.cpp
@@ -126,7 +126,7 @@ Bateau_t Plateau_t::addBateau(int type) {
     int x_in, x_grid, y_in, y_grid, choixUserY, testInt = 1;
     char choixUserX;
 
-    //Saisie de la coordonne X du point d'origine du bateau
+    //*****Saisie de la coordonne X du point d'origine du bateau*****
     do {
         std::cout << "Saisir la coordonne d'origine X du bateau (entre A et J) : " << std::endl; 
         std::cin >> choixUserX;
@@ -141,49 +141,164 @@ Bateau_t Plateau_t::addBateau(int type) {
     x_grid = x_in - 1;
 
 
-    //Saisie de la coordonne Y du point d'origine du bateau
-    bool check = true;
+    //*****Saisie de la coordonne Y du point d'origine du bateau*****
+    bool checkY = true;
     do {
         std::cout << "Saisir la coordonne d'origine Y du bateau (entre 1 et 10) : " << std::endl; 
         std::cin >> choixUserY;
         y_in = choixUserY;
         y_grid = y_in - 1;
-        check = true;
+        checkY = true;
 
         //On vérifie que la coordonnée Y est bien sur la grille
         if(!(choixUserY >= 1 && choixUserY <= 10)) {
             std::cout << "Y : " << choixUserY << std::endl;
             std::cout << "Incorrect, saisir un nombre entre 1 et 10" << std::endl;
-            check = false;
+            checkY = false;
         }
 
         //On vérifie si la case ciblée n'est pas déjà prise par un autre bateau
         else if (this->grid[x_grid][y_grid].getState() == bateau) {
             std::cout << "Incorrect, la case est deja prise par un autre bateau" << std::endl;
-            check = false;
+            checkY = false;
         }
 
         //On vérifie qu'il n'y a pas de bateau autour de la case ciblée
-        else if ((this->grid[x_grid + 1][y_grid].getState() == bateau) ||
-                 (this->grid[x_grid + 1][y_grid + 1].getState() == bateau) ||
-                 (this->grid[x_grid][y_grid + 1].getState() == bateau) ||
-                 (this->grid[x_grid - 1][y_grid + 1].getState() == bateau) ||
-                 (this->grid[x_grid - 1][y_grid].getState() == bateau) ||
-                 (this->grid[x_grid - 1][y_grid - 1].getState() == bateau) ||
-                 (this->grid[x_grid][y_grid - 1].getState() == bateau) ||
-                 (this->grid[x_grid + 1][y_grid - 1].getState() == bateau)) {
+        else if ((this->grid[x_grid + 1][y_grid].getState() == bateau) || //d
+                 (this->grid[x_grid + 1][y_grid + 1].getState() == bateau) || //dh
+                 (this->grid[x_grid][y_grid + 1].getState() == bateau) || //h
+                 (this->grid[x_grid - 1][y_grid + 1].getState() == bateau) || //hg
+                 (this->grid[x_grid - 1][y_grid].getState() == bateau) || //g
+                 (this->grid[x_grid - 1][y_grid - 1].getState() == bateau) || //gb
+                 (this->grid[x_grid][y_grid - 1].getState() == bateau) || //b
+                 (this->grid[x_grid + 1][y_grid - 1].getState() == bateau)) { //bd
 
             std::cout << "Incorrect, bateau a moins d'une case" << std::endl;
-            check = false;
+            checkY = false;
         }
 
-    } while(check == false);
+    } while(checkY == false);
 
 
-    //Saisie de la direction du bateau à partir du point d'origine
-    std::cout << "Saisir la direction de votre bateau (d, g, b, h) :" << std::endl;
+
+    //*****Saisie de la direction du bateau à partir du point d'origine*****
+    int longueur_bateau = 0;
+    switch (type) // 0,1,2,3,4
+        {
+            case porte_avion: 
+                longueur_bateau = 5; break;
+            case croiseur:
+                longueur_bateau = 4; break;
+            case contre_torpilleur_1: 
+                longueur_bateau = 3; break;
+            case contre_torpilleur_2: 
+                longueur_bateau = 3; break;
+            case torpilleur: 
+                longueur_bateau = 2; break;
+            default: longueur_bateau = 3; break;
+        }
+
+    bool checkDir = true;
     char direction = ' ';
-    std::cin >> direction;
+    do {
+        std::cout << "Saisir la direction de votre bateau (d, g, b, h) de taille ";
+        std::cout << longueur_bateau << " :" << std::endl;
+        std::cin >> direction;
+        checkDir = true;
+
+        //Maj des cases du bateau en fonction de la direction choisie
+        if(direction == 'g') //gauche
+        {
+            for (int i = 1; i <= longueur_bateau; i++) {
+                //std::cout << "(" << x_grid - i + 1 << ", " << y_grid + 1 << ")." << std::endl;
+                if (x_grid - i + 1 < 0) // X < A
+                {
+                    checkDir = false;
+                    std::cout << "Erreur, bordure gauche depassee case : (";
+                    std::cout << x_grid - i + 2 << ", " << y_grid + 1 << ")." << std::endl;
+                }
+                else if (((this->grid[x_grid - i][y_grid + 1].getState() == bateau) || //bat hg
+                        (this->grid[x_grid - i][y_grid].getState() == bateau) || //bat g
+                        (this->grid[x_grid - i][y_grid - 1].getState() == bateau)) && //bat gb
+                        (x_grid - i + 1 > 0))
+                { 
+                    checkDir = false;
+                    std::cout << "Erreur, bateau a proximite de la case : (";
+                    std::cout << x_grid - i + 2 << ", " << y_grid + 1 << ")." << std::endl;
+                }
+            }
+        }
+        else if(direction == 'b') //bas 
+        {
+            for (int i = 1; i <= longueur_bateau; i++)
+            {
+                //std::cout << "(" << x_grid + 1 << ", " << y_grid + i + 1 << ")." << std::endl;
+                if (y_grid + i - 1 > 9) 
+                {
+                    checkDir = false;
+                    std::cout << "Erreur, bordure basse depassee case : (";
+                    std::cout << x_grid + 1 << ", " << y_grid + i << ")." << std::endl;
+                }
+                else if ( (this->grid[x_grid - 1][y_grid + i].getState() == bateau) || //gb
+                        (this->grid[x_grid][y_grid + i].getState() == bateau) || //b
+                        (this->grid[x_grid + 1][y_grid + i].getState() == bateau)) //bd
+                { 
+                    checkDir = false;
+                    std::cout << "Erreur, bateau a proximite de la case : (";
+                    std::cout << x_grid + 1 << ", " << y_grid + i << ")." << std::endl; 
+                }
+            }
+        }
+        else if(direction == 'd') //droite
+        {
+            for (int i = 1; i <= longueur_bateau; i++)
+            {
+                //std::cout << "(" << x_grid + i + 1 << ", " << y_grid + 1 << ")." << std::endl;
+                if (x_grid + i - 1 > 9)
+                {
+                    checkDir = false;
+                    std::cout << "Erreur, bordure droite depassee case : (";
+                    std::cout << x_grid + i << ", " << y_grid + 1 << ")." << std::endl;
+                }
+                else if ((this->grid[x_grid + i][y_grid].getState() == bateau) || //d
+                        (this->grid[x_grid + i][y_grid + 1].getState() == bateau) || //dh
+                        (this->grid[x_grid + i][y_grid - 1].getState() == bateau)) //bd
+                { 
+                    checkDir = false;
+                    std::cout << "Erreur, bateau a proximite de la case : (";
+                    std::cout << x_grid + i << ", " << y_grid + 1 << ")." << std::endl; 
+                }
+                
+            }
+        }
+        else if(direction == 'h') //haut
+        {
+            for (int i = 1; i <= longueur_bateau; i++)
+            {
+                //std::cout << "(" << x_grid + 1 << ", " << y_grid - i + 1 << ")." << std::endl;   
+                if (y_grid - i + 1 < 0)
+                {
+                    checkDir = false;
+                    std::cout << "Erreur, bordure haute depassee case : (";
+                    std::cout << x_grid + 1 << ", " << y_grid - i + 1 << ")." << std::endl;
+                }
+                else if ( (this->grid[x_grid + 1][y_grid - i].getState() == bateau) || //dh
+                        (this->grid[x_grid][y_grid - i].getState() == bateau) || //h
+                        (this->grid[x_grid - 1][y_grid - i].getState() == bateau)) //hg
+                {
+                    checkDir = false;
+                    std::cout << "Erreur, bateau a proximite de la case : (";
+                    std::cout << x_grid + 1 << ", " << y_grid - i + 2 << ")." << std::endl; 
+                }
+            }
+        }
+        else
+        {
+            checkDir = false;
+            std::cout << "Erreur, aucune des lettres en minuscule n'a ete saisie !" << std::endl;  
+        }
+
+    } while(checkDir == false);
 
     //Creation du bateau après avoir vérifié son emplacement
     Bateau_t bateau0(type, x_grid, y_grid, direction);
